fc1a70713ec191788495c488e921e618d4a0706c
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-10 09:39:59 +0200

    Basic content and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,77 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import { useInView } from 'react-intersection-observer';
 
-export default function Home() {
+import {
+  NoddingWoman,
+  JackBlack,
+  Oprah,
+  TracyMorgan,
+  WorkingPenguin,
+} from '@/components/gifs/gifs';
+
+export default function HomePage() {
+  const { ref: myRef, inView: elementIsVisible } = useInView();
+  console.log('elementIsVisible', elementIsVisible);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main className='mother-grid'>
+        <header className='header' id={elementIsVisible ? 'scrolling' : ''}>
+          <h1>Magefølelsen</h1>
+          <h2>Kommunikasjon & design</h2>
+          <a href='mailto:hei@magefolelsen.no'>hei@magefolelsen.no</a>
+        </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <article className='questions'>
+          <h3 className='questions-title'>Skal vi jobbe sammen?</h3>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <div className='questions-items' ref={myRef}>
+            <section>
+              <p>
+                Er du enig i at man først må ha en god historie og et mål med
+                den historien?
+              </p>
+              <NoddingWoman />
+            </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <section>
+              <p>
+                Og med historien og målet på plass så kan man jobbe med hvordan
+                historien fortelles?
+              </p>
+              <JackBlack />
+            </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+            <section>
+              <p>
+                Ditt design og dine nettsider bør vel være tilpasset din
+                historiefortelling og dine mål?
+              </p>
+              <Oprah />
+            </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <section>
+              <p>
+                Altså: En god historie og klare mål, med en god fortelling, gir
+                de beste nettsidene?
+              </p>
+              <TracyMorgan />
+            </section>
+
+            <section>
+              <p>
+                Om alt dette gir mening så er det vel bare å begynne jobben
+                sammen. Vi snakkes!
+              </p>
+              <WorkingPenguin />
+            </section>
+          </div>
+        </article>
+
+        <section className='about'>
+          <a href='./om-magefolelsen'>Om magefølelsen</a>
+        </section>
+      </main>
+    </>
+  );
 }
---
+++ components/gifs/gifs.tsx
@@ -0,0 +1,93 @@
+import Image from 'next/image';
+import styles from './gifs.module.css';
+
+import noddingWoman from '@/public/gifs/nodding-woman.gif';
+import jackBlack from '@/public/gifs/jack-black.gif';
+import oprah from '@/public/gifs/oprah.gif';
+import tracyMorgan from '@/public/gifs/tracy-morgan.gif';
+import workingPenguin from '@/public/gifs/working-penguin.gif';
+
+export function NoddingWoman() {
+  return (
+    <>
+      <picture>
+        <Image
+          className={styles.gifs}
+          src={noddingWoman}
+          alt='Nikkende dame er enig'
+          width={200}
+          height={200}
+          unoptimized={true}
+        />
+      </picture>
+    </>
+  );
+}
+
+export function JackBlack() {
+  return (
+    <>
+      <picture>
+        <Image
+          className={styles.gifs}
+          src={jackBlack}
+          alt='Komiker Jack Black nikker enig, toppløs'
+          width={200}
+          height={200}
+          unoptimized={true}
+        />
+      </picture>
+    </>
+  );
+}
+
+export function Oprah() {
+  return (
+    <>
+      <picture>
+        <Image
+          className={styles.gifs}
+          src={oprah}
+          alt='Oprah Winfrey gråter av glede'
+          width={200}
+          height={200}
+          unoptimized={true}
+        />
+      </picture>
+    </>
+  );
+}
+
+export function TracyMorgan() {
+  return (
+    <>
+      <picture>
+        <Image
+          className={styles.gifs}
+          src={tracyMorgan}
+          alt='Komiker Tracy Morgan er veldig enig'
+          width={200}
+          height={200}
+          unoptimized={true}
+        />
+      </picture>
+    </>
+  );
+}
+
+export function WorkingPenguin() {
+  return (
+    <>
+      <picture>
+        <Image
+          className={styles.gifs}
+          src={workingPenguin}
+          alt='En lliten pingvin på vei til jobb'
+          width={200}
+          height={200}
+          unoptimized={true}
+        />
+      </picture>
+    </>
+  );
+}
