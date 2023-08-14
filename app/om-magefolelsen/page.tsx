1fc847c03f726e99e55a9b31829d2ae4dafc326f
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-14 17:40:22 +0200

    Finished content work for today

--- app/om-magefolelsen/page.tsx
+++ app/om-magefolelsen/page.tsx
@@ -9,30 +9,30 @@ export default function AboutPage() {
         <section>
           <h3>Hvorfor Magefølelsen?</h3>
           <p>
-            Ja, hvorfor heter vi Magefølelsen, et litt pretensiøst ord og begrep
-            i seg selv? Vel, det får vi leve med, for begrunnelsen er ganske
-            enkel. Et sted mellom følelser og logikk ligger nemlig magefølelsen,
-            og det synes vi er et veldig bra sted å jobbe når man skal drive
-            kommunikasjon, og lage godt design.
+            Ja, hvorfor heter vi Magefølelsen? Det er et litt pretensiøst ord og
+            begrep i seg selv? Vel, det får vi leve med, for begrunnelsen er
+            ganske enkel. Et sted mellom følelser og logikk ligger nemlig
+            magefølelsen, og det synes vi er et veldig bra sted å jobbe når man
+            skal drive kommunikasjon og lage godt design.
           </p>
         </section>
         <section>
           <h3>Hvordan jobber vi?</h3>
           <p>
             Rett kommunikasjon og design til rett kunde, til rett tid. Så
-            enkelt, og så himla vanskelig. Det er alltid morsomst å drive med
-            det grafiske når det kommer til design av nettsider – farger,
-            fonter, bevegelser, etc. Men det krever en grundig analyse av dine
-            ønsker og mål, opp mot en nå-situasjon av hele din aktivitet.
-            Kanskje du har lyst på en Rolls Royce, og det skal du få hvis du
-            klarer å håndtere den, men hvis det du egentlig trenger er en solid
-            Toyota, er det det du får. Og du kommer til å bli fornøyd med den.
+            enkelt, og så himla vanskelig. Det er alltid morsomst når man får se
+            skissene og hvordan sidene kommer til å se ut. Men for å komme dit
+            kreves det en grundig analyse av dine ønsker og mål, opp mot en
+            nå-situasjon av hele din aktivitet. Kanskje du har lyst på en Rolls
+            Royce, og det skal du få hvis du klarer å håndtere den, men hvis det
+            du egentlig trenger er en solid Toyota, er det det du får, med litt
+            RR-bling på toppen.
           </p>
         </section>
         <section>
           <h3>Hvem er vi?</h3>
           <p>
-            Hei! Jeg er Ådne, og jeg har blant annet jobbet som profesjonell
+            Hei! Jeg heter Ådne, og har blant annet jobbet som profesjonell
             danser i Storbritannia, salgsleder i Aftenposten, fagkonsulent i
             Olympiatoppen, Nav-konsulent, og kommunikasjonsrådgiver. Nå har jeg
             pisket meg selv til å bli utvikler, og jobber med ulike designere og
