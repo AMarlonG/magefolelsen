26dda1aa15051134179f7047b8fc27aa18ae6f04
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-14 17:28:09 +0200

    Added content to about page

--- app/om-magefolelsen/page.tsx
+++ app/om-magefolelsen/page.tsx
@@ -1,7 +1,48 @@
 export default function AboutPage() {
   return (
     <>
-      <h1>Hi!</h1>
+      <h1>Om Magefølelsen</h1>
+      <h2>Hvorfor Magefølelsen?</h2>
+      <h2>Hvordan jobber vi?</h2>
+      <h2>Hvem er vi?</h2>
+      <article>
+        <section>
+          <h3>Hvorfor Magefølelsen?</h3>
+          <p>
+            Ja, hvorfor heter vi Magefølelsen, et litt pretensiøst ord og begrep
+            i seg selv? Vel, det får vi leve med, for begrunnelsen er ganske
+            enkel. Et sted mellom følelser og logikk ligger nemlig magefølelsen,
+            og det synes vi er et veldig bra sted å jobbe når man skal drive
+            kommunikasjon, og lage godt design.
+          </p>
+        </section>
+        <section>
+          <h3>Hvordan jobber vi?</h3>
+          <p>
+            Rett kommunikasjon og design til rett kunde, til rett tid. Så
+            enkelt, og så himla vanskelig. Det er alltid morsomst å drive med
+            det grafiske når det kommer til design av nettsider – farger,
+            fonter, bevegelser, etc. Men det krever en lang og grundig analyse
+            av dine ønsker og mål, opp mot en nå-situasjon av hele din
+            aktivitet. Kanskje du har lyst på en Rolls Royce, og det skal du få
+            hvis du klarer å håndtere den, men hvis det du egentlig trenger er
+            en solid Toyota, er det det du får. Og du kommer til å bli fornøyd
+            med den.
+          </p>
+        </section>
+        <section>
+          <h3>Hvem er vi?</h3>
+          <p>
+            Hei! Jeg er Ådne, og jeg har blant annet jobbet som profesjonell
+            danser i Storbritannia, salgsleder i Aftenposten, fagkonsulent i
+            Olympiatoppen, Nav-konsulent, og kommunikasjonsrådgiver. Nå har jeg
+            pisket meg selv til å bli utvikler, og jobber med ulike designere og
+            utviklere ut ifra hva hvert prosjekt trenger. Med andre ord, de
+            rette folkene brukes til ditt prosjekt, og jeg skal styre skuta i
+            havn. Vi snakkes!
+          </p>
+        </section>
+      </article>
     </>
   );
 }
