383782071f295b46f1dbac5e492a2dda4c5a0f90
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-23 07:30:54 +0200

    Added spacing

--- app/om-magefolelsen/page.tsx
+++ app/om-magefolelsen/page.tsx
@@ -45,9 +45,9 @@ export default function AboutPage() {
               ... med litt RR attåt. 😎
             </p>
             <p>
-              Noen vi har jobbet med er:
-              <a href='https://www.dansenshus.com/'>Dansens Hus</a> og
-              <a href='https://www.baerumkulturhus.no/'>Bærum Kulturhus</a>.
+              Noen vi har jobbet med er:{' '}
+              <a href='https://www.dansenshus.com/'> Dansens Hus</a> og
+              <a href='https://www.baerumkulturhus.no/'> Bærum Kulturhus</a>.
             </p>
           </section>
           <section id='who'>
